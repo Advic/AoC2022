d58390f76d26ae0ebf62e5b4e31828a53f0df890
Advic <loktinmartinho@gmail.com>
2022-12-04 21:35:09 -0800

    Minor typo

--- template/test_dayDAYNUM.py
+++ template/test_dayDAYNUM.py
@@ -11,7 +11,7 @@ PART2_EXPECTED = None
 
 class TestDayDAYNUM(unittest.TestCase):
     def test_part1(self):
-        self.assertEqual(PART1_EXPECTED, par1(TEST_DATA))
+        self.assertEqual(PART1_EXPECTED, part1(TEST_DATA))
 
     def test_part2(self):
         self.assertEqual(PART2_EXPECTED, part2(TEST_DATA))
